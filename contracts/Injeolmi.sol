5fd709e330c217136b4d1efcb98e72b1e9229cb4
TheGreatHB <kavanaboy@gmail.com>
2021-11-04 02:36:27 +0900

    Update Injeolmi.sol

--- contracts/Injeolmi.sol
+++ contracts/Injeolmi.sol
@@ -11,18 +11,26 @@ import "./interfaces/IInjeolmi.sol";
 contract Injeolmi is Ownable, Pausable, IInjeolmi {
     using SafeMath for uint256;
 
-    string constant public NAME = "Injeolmi";
-    string constant public SYMBOL = "IJM";
+    string public constant NAME = "Injeolmi";
+    string public constant SYMBOL = "IJM";
 
     // 1억개 발행, 추가 발행 없음
-    uint256 constant public COIN = 100000000 ** uint256(DECIMALS);
-    uint8 constant public DECIMALS = 8;
+    uint256 public constant COIN = 100000000**uint256(DECIMALS);
+    uint8 public constant DECIMALS = 8;
+    uint256 private constant MULTIPLIER = 10**9;
 
-    uint256 private _totalDist;
+    struct UserInfo {
+        uint256 lastBalance;
+        uint256 lastMultiplier;
+        uint256 resettingCount;
+    }
 
-    mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowed;
-    mapping(address => bool) private excluded;
+    mapping(address => UserInfo) public _userInfo;
+    mapping(address => mapping(address => uint256)) public allowed;
+    mapping(address => bool) public excluded;
+
+    uint256 public accMultiplier = MULTIPLIER;
+    uint256 public resettingCount;
 
     function exclude(address addr) external onlyOwner {
         excluded[addr] = true;
@@ -32,36 +40,161 @@ contract Injeolmi is Ownable, Pausable, IInjeolmi {
         excluded[addr] = false;
     }
 
-    function name() external pure returns (string memory) { return NAME; }
-    function symbol() external pure returns (string memory) { return SYMBOL; }
-    function decimals() external pure returns (uint8) { return DECIMALS; }
-    function totalSupply() external pure returns (uint256) { return COIN; }
+    function name() external pure returns (string memory) {
+        return NAME;
+    }
+
+    function symbol() external pure returns (string memory) {
+        return SYMBOL;
+    }
+
+    function decimals() external pure returns (uint8) {
+        return DECIMALS;
+    }
+
+    function totalSupply() external pure returns (uint256) {
+        return COIN;
+    }
 
     constructor() public {
         _userInfo[msg.sender].lastBalance = COIN;
+        _userInfo[msg.sender].lastMultiplier = MULTIPLIER;
+    }
+
+    function updateMultiplier(address[] calldata _addresses) external onlyOwner whenPaused {
+        uint256 length = _addresses.length;
+        uint256 _accMultiplier = accMultiplier;
+        for (uint256 i = 0; i < length; i++) {
+            UserInfo storage _uInfo = _userInfo[_addresses[i]];
+            uint256 _lastMultiplier = _uInfo.lastMultiplier;
+
+            if (_uInfo.resettingCount == resettingCount + 1 || _lastMultiplier == 0) {} else if (_lastMultiplier == _accMultiplier) {
+                _uInfo.resettingCount = resettingCount + 1;
+            } else {
+                _uInfo.lastBalance = _uInfo.lastBalance.mul(_accMultiplier).div(_lastMultiplier);
+                _uInfo.lastMultiplier = MULTIPLIER;
+                _uInfo.resettingCount = resettingCount + 1;
+            }
+        }
+    }
+
+    function finishUpdatingMultiplier() external onlyOwner whenPaused {
+        accMultiplier = MULTIPLIER;
+        resettingCount = resettingCount + 1;
     }
 
     function balanceOf(address user) external view returns (uint256 balance) {
-        uint256 balance = balances[user];
-        return balance.add(_totalDist.mul(balance).div(COIN));
+        UserInfo memory _uInfo = _userInfo[user];
+        if (_uInfo.lastBalance == 0 || _uInfo.lastMultiplier == 0) return 0;
+        return _uInfo.lastBalance.mul(accMultiplier).div(_uInfo.lastMultiplier);
     }
 
-    function _transfer(address from, address to, uint256 amount) private {
-        uint256 realAmount = amount.mul(COIN).div(COIN.add(_totalDist));
-        balances[from] = balances[from].sub(realAmount);
-        if (excluded[from] != true) {
+    function updateBalance(address user) public returns (uint256, uint256) {
+        UserInfo storage _uInfo = _userInfo[user];
+        uint256 _accMultiplier = accMultiplier;
+        uint256 _lastBalance = _uInfo.lastBalance;
+        uint256 _lastMultiplier = _uInfo.lastMultiplier;
 
-            // 9% 떡돌리기
-            uint256 dist = realAmount.mul(9).div(100);
-            // 1% 떡방앗간 팁
-            uint256 fee = realAmount.div(100);
-
-            _totalDist = _totalDist.add(dist);
-            realAmount = realAmount.sub(dist).sub(fee);
-            balances[owner()] = balances[owner()].add(fee);
+        if (_lastBalance == 0) {
+            return (0, _accMultiplier);
+        } else if (_lastMultiplier != _accMultiplier) {
+            _uInfo.lastBalance = _uInfo.lastBalance.mul(_accMultiplier).div(_lastMultiplier);
+            _uInfo.lastMultiplier = _accMultiplier;
         }
-        balances[to] = balances[to].add(realAmount);
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) private {
+        UserInfo storage _fromInfo = _userInfo[from];
+        UserInfo storage _toInfo = _userInfo[to];
+
+        uint256 _accMultiplier = accMultiplier;
+
+        uint256 fromUpdatedBalance;
+        {
+            //avoids stack too deep errors
+            uint256 fromLastBalance = _fromInfo.lastBalance;
+            uint256 fromLastMultiplier = _fromInfo.lastMultiplier;
+            fromUpdatedBalance = fromLastBalance;
+            if (fromLastBalance != 0 && fromLastMultiplier != _accMultiplier) {
+                fromUpdatedBalance = fromLastBalance.mul(_accMultiplier).div(fromLastMultiplier);
+            }
+        }
+
+        uint256 toUpdatedBalance;
+        {
+            //avoids stack too deep errors
+            uint256 toLastBalance = _toInfo.lastBalance;
+            uint256 toLastMultiplier = _toInfo.lastMultiplier;
+            toUpdatedBalance = toLastBalance;
+            if (toLastBalance != 0 && toLastMultiplier != _accMultiplier) {
+                toUpdatedBalance = toLastBalance.mul(_accMultiplier).div(toLastMultiplier);
+            }
+        }
+
+        if (amount == uint256(-1)) amount = fromUpdatedBalance; //TODO: check
+
+        if (excluded[from]) {
+            _fromInfo.lastBalance = fromUpdatedBalance.sub(amount);
+            _toInfo.lastBalance = toUpdatedBalance.add(amount);
+        } else {
+            // 9% 떡돌리기
+            uint256 dist = amount.mul(9).div(100);
+            // 1% 떡방앗간 팁
+            uint256 fee = amount.div(100);
+
+            uint256 thisMultiplier = COIN.mul(MULTIPLIER).div(COIN.sub(dist));
+
+            UserInfo storage _ownerInfo = _userInfo[address(owner())];
+            uint256 ownerUpdatedBalance;
+            {
+                //avoids stack too deep errors
+                uint256 ownerLastBalance = _ownerInfo.lastBalance;
+                uint256 ownerLastMultiplier = _ownerInfo.lastMultiplier;
+                ownerUpdatedBalance = ownerLastBalance;
+                if (ownerLastBalance != 0 && ownerLastMultiplier != _accMultiplier) {
+                    ownerUpdatedBalance = ownerLastBalance.mul(_accMultiplier).div(ownerLastMultiplier);
+                }
+            }
+            {
+                //avoids stack too deep errors
+                (_fromInfo.lastBalance, _toInfo.lastBalance, _ownerInfo.lastBalance) = __transfer(fromUpdatedBalance, toUpdatedBalance, ownerUpdatedBalance, amount, dist, fee, thisMultiplier);
+            }
+
+            _accMultiplier = _accMultiplier.mul(thisMultiplier).div(MULTIPLIER);
+            accMultiplier = _accMultiplier;
+            _ownerInfo.lastMultiplier = _accMultiplier;
+        }
+
+        _fromInfo.lastMultiplier = _accMultiplier;
+        _toInfo.lastMultiplier = _accMultiplier;
+
         emit Transfer(from, to, amount);
+    }
+
+    function __transfer(
+        uint256 fromUB,
+        uint256 toUB,
+        uint256 oUB,
+        uint256 amount,
+        uint256 dist,
+        uint256 fee,
+        uint256 thisMultiplier
+    )
+        private
+        pure
+        returns (
+            uint256 f,
+            uint256 t,
+            uint256 o
+        )
+    {
+        f = (fromUB.sub(amount)).mul(thisMultiplier).div(MULTIPLIER);
+        t = (toUB.add(amount.sub(dist.add(fee)))).mul(thisMultiplier).div(MULTIPLIER);
+        o = (oUB.add(fee)).mul(thisMultiplier).div(MULTIPLIER);
     }
 
     function transfer(address to, uint256 amount) public whenNotPaused returns (bool success) {
@@ -79,7 +212,11 @@ contract Injeolmi is Ownable, Pausable, IInjeolmi {
         return allowed[user][spender];
     }
 
-    function transferFrom(address from, address to, uint256 amount) external whenNotPaused returns (bool success) {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external whenNotPaused returns (bool success) {
         uint256 _allowance = allowed[from][msg.sender];
         if (_allowance != uint256(-1)) {
             allowed[from][msg.sender] = _allowance.sub(amount);
